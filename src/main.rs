2af2cb8e152a3bae2c41b7727f748a0edff44081
pjmp <11157420+pjmp@users.noreply.github.com>
2021-07-07 22:42:12 +0545

    refactor stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
 use anyhow::{Context, Result};
+use clap::{crate_description, crate_name, crate_version, App, AppSettings, Arg, SubCommand};
 
-mod cli;
-mod core;
+mod updater;
 
 fn main() -> Result<()> {
-    let app = cli::new().get_matches();
+    let app = App::new(crate_name!())
+        .bin_name("cargo")
+        .subcommand(
+            SubCommand::with_name("updater")
+                .setting(AppSettings::ColorAuto)
+                .version(crate_version!())
+                .about(crate_description!())
+                .args(&[
+                    Arg::from_usage("-u --update 'Update upgradable crates'")
+                        .conflicts_with("list"),
+                    Arg::from_usage("-l --list 'List latest available version'")
+                        .conflicts_with("update"),
+                ]),
+        )
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .get_matches();
 
-    let cmd = match app.subcommand {
-        None => {
-            let _ = cli::new().print_long_help();
-            std::process::exit(1);
+    if let Some(cmd) = app.subcommand {
+        let cmd = cmd.matches;
+
+        let container = updater::CratesInfoContainer::new()?;
+
+        if cmd.is_present("list") || cmd.args.is_empty() {
+            container
+                .list()
+                .context("Unable to list installed binaries.")?;
         }
-        Some(sub_cmd) => sub_cmd.matches,
-    };
 
-    let container = core::CratesInfoContainer::new()?;
-
-    if cmd.is_present("list") || cmd.args.is_empty() {
-        container
-            .list()
-            .context("Unable to list installed binaries.")?;
-    }
-
-    if cmd.is_present("update") {
-        container.update().context("Unable to run updater.")?;
+        if cmd.is_present("update") {
+            container.update().context("Unable to run updater.")?;
+        }
     }
 
     Ok(())
